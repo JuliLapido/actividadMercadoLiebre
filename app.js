01582953a4b9f22b53457b484a96611d917ab44f
Julián Lapido <106562977+JuliLapido@users.noreply.github.com>
2022-07-18 20:23:04 -0300

    Update app.js
    agrego port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const server = express();
 const path = require('path');
-const port = 3000;
+const port = process.env.PORT || 3000;
 const start = () => console.log('Starting express server...');  
 
 server.listen(port,start());
